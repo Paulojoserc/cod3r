1949f3cb2bafb6726e279d3cae4a93afbdf13dea
Paulojoserc <pjrc83@gmail.com>
2022-09-22 16:07:46 -0300

    Enum

--- src/oo/composicao/desafio/TesteCompra.java
+++ src/oo/composicao/desafio/TesteCompra.java
@@ -1,17 +1,24 @@
 package oo.composicao.desafio;
 
-import oo.composicao.Compra;
-import oo.composicao.Item;
+
 
 public class TesteCompra {
 public static void main(String[] args) {
-	Cliente c1 = new Cliente(null);
-	c1.name = "João Pedro";
-	c1.compras.add()
-	c1.itens.add(new Item("Caneta", 3, 18.79));
+	
+	
+	Compra compra1 = new Compra();
+	compra1.adicionarItem("Caneta", 1, 100);
+	compra1.adicionarItem(new Produto("Notebook", 2000.00 ),2);
+	
+	Compra compra2 = new Compra();
+	compra2.adicionarItem("Caderno", 10, 10);
+	compra2.adicionarItem(new Produto("Impressora", 1000.00 ),1);
 
-	System.out.println(c1.itens.size());
-	System.out.println(c1.obterValorTotal());
-
+	Cliente cliente = new Cliente("Maria Julia Moraes");
+	cliente.adicionarCompra(compra1);
+	cliente.compras.add(compra2);
+	
+	System.out.println(cliente.obterValorTotal());
+	
 }
 }
