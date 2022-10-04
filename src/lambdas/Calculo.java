8ea315066f84fd37880c0b63d9409dc6d463a070
Paulojoserc <pjrc83@gmail.com>
2022-10-04 16:35:16 -0300

    Função Lambda

---
+++ src/lambdas/Calculo.java
@@ -0,0 +1,6 @@
+package lambdas;
+
+public interface Calculo {
+
+	public abstract double exercutar(double a, double b);
+}
---
+++ src/lambdas/Somar.java
@@ -0,0 +1,10 @@
+package lambdas;
+
+public class Somar implements Calculo{
+
+	public double exercutar(double a, double b) {
+	return a + b;
+}
+
+
+}
