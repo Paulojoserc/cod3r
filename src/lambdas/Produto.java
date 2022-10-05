1c4ba9f905fae32ef634a2f2cebbc2df93b5ea13
Paulojoserc <pjrc83@gmail.com>
2022-10-04 22:48:21 -0300

    Predicate 01

---
+++ src/lambdas/Produto.java
@@ -0,0 +1,15 @@
+package lambdas;
+
+public class Produto {
+	final String name;
+	final double preco;
+	final double desconto;
+
+	public Produto(String name, double preco, double desconto) {
+		
+		this.name = name;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+
+}
