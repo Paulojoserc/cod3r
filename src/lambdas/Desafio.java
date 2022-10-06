8d20dd1736d504e2536f21dadf9f9e1bea808a4b
Paulo Costa <pjrc83@gmail.com>
2022-10-06 17:28:38 -0300

    List/Iterator/Stream

--- src/lambdas/Desafio.java
+++ src/lambdas/Desafio.java
@@ -6,17 +6,21 @@ import java.util.function.UnaryOperator;
 
 public class Desafio {
 	public static void main(String[] args) {
-Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US);
 
+		/*
+		 *  1. A partir do produto calcular o preço real (com desconto)
+		 *  2. Imposto Municipal: >= 2500 (8,5%)/ <2500 (Isento) 
+		 *  3. Frete: >= 3000 (100)/ <3000 (50)
+		 *  4.Arredondar: Deixando duas casas decimais
+		 *  5. Formatar: R$1234,56
+		 */
+		
 		Function<Produto, Double> precoFinal = produto -> produto.preco * (1 - produto.desconto);
-		UnaryOperator<Double> impostoMunicipal =
-		preco -> preco >= 2500 ? preco * 1.085 : preco;
-		UnaryOperator < Double > frete= 
-				preco -> preco >= 3000 ? preco + 100 : preco +50;
-		UnaryOperator<Double> arredondar= 
-				preco -> Double.parseDouble(String.format("%.2f", preco));
-		Function<Double, String> formatar= 
-				preco -> ("R$"+preco).replace(".", ",");
+		UnaryOperator<Double> impostoMunicipal = preco -> preco >= 2500 ? preco * 1.085 : preco;
+		UnaryOperator<Double> frete = preco -> preco >= 3000 ? preco + 100 : preco + 50;
+		UnaryOperator<Double> arredondar = preco -> Double.parseDouble(String.format("%.2f", preco));
+		Function<Double, String> formatar = preco -> ("R$" + preco).replace(".", ",");
 
 		Produto p = new Produto("iPad", 3235.89, 0.13);
 
